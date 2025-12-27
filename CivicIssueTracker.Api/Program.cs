4e9d84503c95c6280747cefa0fc323c85619f233
anurag-kokil <anurag.kokil25@gmail.com>
2025-12-28 01:25:45 +0530

    Implement JWT authentication with user registration and login functionality #1

--- CivicIssueTracker.Api/Program.cs
+++ CivicIssueTracker.Api/Program.cs
@@ -1,21 +1,60 @@
 using CivicIssueTracker.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using CivicIssueTracker.Api.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
+
+// Controllers
+builder.Services.AddControllers();
+
+// Database
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// JWT Authentication
+builder.Services.AddScoped<JwtTokenHelper>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+            )
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-// Middleware
+// Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Middleware Order Matters
 app.UseHttpsRedirection();
+// Enable authentication and authorization
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Map controllers
+app.MapControllers();
 
 app.Run();
