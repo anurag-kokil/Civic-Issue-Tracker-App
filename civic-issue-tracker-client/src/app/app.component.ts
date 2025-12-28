19d6baaf232b772304202e1f31646594c628539c
anurag-kokil <anurag.kokil25@gmail.com>
2025-12-28 14:51:36 +0530

    feat: implement dynamic navbar visibility and logout functionality

--- civic-issue-tracker-client/src/app/app.component.ts
+++ civic-issue-tracker-client/src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterModule],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'civic-issue-tracker-client';
+
+  constructor(
+    public auth: AuthService,
+    private router: Router
+  ) {}
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+
+  showNavbar(): boolean {
+    return this.auth.isLoggedIn() && this.router.url !== '/login';
+  }
 }
